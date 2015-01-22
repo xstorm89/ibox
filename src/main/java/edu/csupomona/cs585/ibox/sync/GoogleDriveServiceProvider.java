92c9a60a23b024260032b015566c950ab2aade4c
xstorm89 <xuxuncharles@gmail.com>
2015-01-21 17:10:21 -0800

    1

--- src/main/java/edu/csupomona/cs585/ibox/sync/GoogleDriveServiceProvider.java
+++ src/main/java/edu/csupomona/cs585/ibox/sync/GoogleDriveServiceProvider.java
@@ -17,8 +17,8 @@ import com.google.api.services.drive.DriveScopes;
 
 public class GoogleDriveServiceProvider {
 
-	private static String CLIENT_ID = "PASTE_YOUR_CLIENT_ID_HERE";
-	private static String CLIENT_SECRET = "PASTE_YOUR_CLIENT_SECRET_HERE";
+	private static String CLIENT_ID = "563307032460-ia00so2gpa0ksf7embhu101dj4uncraf.apps.googleusercontent.com";
+	private static String CLIENT_SECRET = "wN5y7jrXF6ortmzKnYFOBNut";
 
 	private static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
 
